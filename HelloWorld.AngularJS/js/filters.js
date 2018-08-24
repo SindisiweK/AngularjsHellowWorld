36d64457e79a943fe338d3da017e61b77484d9ce
SindisiweK <SindisiweK@sahomeloans.com>
2018-08-24 11:15:42 +0200

    startup of angularjs hellowworld

---
+++ HelloWorld.AngularJS/js/filters.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+    angular
+        .module("demoApp")
+        .filter('sortByFirstName', function () {
+                return function (array) {
+                    function compare(a, b) {
+                        return a.firstName.localeCompare(b.firstName)
+                    }
+                    return array.sort(compare);
+                }
+            }
+        );
+})();
---
+++ HelloWorld.AngularJS/js/services.js
@@ -0,0 +1,17 @@
+(function() {
+    angular.module("demoApp").service('userService', function () {
+        var collection = [
+            { firstName: 'Thomas', lastName: 'Ochman' },
+            { firstName: 'Amber', lastName: 'Wilkie' },
+            { firstName: 'Raoul', lastName: 'Diffouo' }
+        ];
+        return {
+            users: function () {
+                return collection;
+            },
+            add: function (object) {
+                collection.push(object);
+            }
+        }
+    });
+})();
